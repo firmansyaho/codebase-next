b02029e765e5b953108a8a75c195bada0f5af1b8
Firmansyah Otoluwa <firmansyaho@primelab.io>
2022-01-25 00:34:31 +0700

    fix unhandledImage requests

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -4,7 +4,7 @@ if (typeof window === "undefined") {
     server.listen();
 } else {
     const { worker } = require("./browser");
-    worker.start();
+    worker.start({ onUnhandledRequest: "bypass" });
 }
 
 export {};
